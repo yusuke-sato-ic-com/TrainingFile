44c92f1c75dc1d54552dba4c3720de569f2eccab
佐藤悠介 <yusuke_sato@ic-com.co.jp>
2016-03-09 18:59:13 +0900

    2016/03/09読み込み、格納
    Fileオブジェクトの位置変更
    while文を1つに
    分割とMapへの格納

--- CalculateSales.java
+++ CalculateSales.java
@@ -1,25 +1,45 @@
-package jp.co.iccom.yusuke_sato.calculate_sales;
+package jp.co.iccom.sato_yusuke.calculate_sales.TrainingFile;
 
 import java.io.*;
+import java.util.*;
 
 public class CalculateSales {
 	public static void main(String[] args) {
 		// コマンドライン引数で、ディレクトリより支店定義ファイルを読み込む
-		// ファイルが存在しない場合はエラーメッセージ "支店定義ファイルが存在しません"を表示
+		// ディレクトリまでのパス指定
+		File file = new File(args[0]); //Fileオブジェクトの生成１
+		File branch = new File(file,"branch.lst"); // Fileオブジェクトの生成２
 
 		try {
-			File file = new File(args[0]);
-			FileReader fr = new FileReader(file); // FileReaderオブジェクトの生成
+			if (!branch.exists()) { // 指定したファイルが存在しているか確認し、false時の処理
+				System.out.println("支店定義ファイルは存在しません");
+				return;
+			}
+			FileReader fr = new FileReader(branch); // FileReaderオブジェクトの生成
 			BufferedReader br = new BufferedReader(fr); // BufferedReaderオブジェクトの生成
 			String line; // ファイルから１行だけ読み込み
-			while((line = br.readLine()) != null) { // 読み込むデータがなくなったらfalse
-				System.out.println(line);
+			Map<String, String> map = new HashMap<String, String>();
+			while ((line = br.readLine()) != null) {
+				String[] branchData = line.split(",");
+				System.out.println(branchData[0]);
+				map.put(branchData[0], branchData[1]);
 			}
 			br.close(); // ストリームを閉じる
 
+		} catch(IOException e) { // 例外を受け取る
+				System.out.println(e);
 
-		} catch(IOException e) {
-			 System.out.println(e);
 		}
 	}
 }
+
+
+/*
+
+Map<String, String> map = new HashMap<String, String>();
+while ((s = br.readLine()) != null) {
+String line = s;
+String[] bra = line.split(",");
+map.put(bra[0], bra[1]);
+
+*/
